6ffc1c65915217290d4ef9b0f6c4a6195d2c7d8f
w. <71177337+Wiktor01@users.noreply.github.com>
2024-10-03 14:16:29 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+author 'DEVHUB (store.devhub.gg)'
+description 'FREE FRUITS PACK 1'
+version '1.0.0'
+
+files {
+    "stream/dh_fruits_1.ytyp",
+}
+
+data_file "DLC_ITYP_REQUEST" "stream/dh_fruits_1.ytyp"
+
+escrow_ignore {
+    'stream/**/*',
+}
+dependency '/assetpacks'
